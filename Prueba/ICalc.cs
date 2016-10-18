6b520eef497b4c71c9471be6da0b37a8537bfa4b
Pablo <Pablo@PABLO_SURFACE>
2016-10-18 11:49:31 +0200

    final

---
+++ Prueba/ICalc.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Prueba
+{
+    public  class Calc
+    {
+        private List<float> _numbers;
+
+        public List<float> numbers
+        {
+            get { return _numbers; }
+            set { _numbers = numbers; } 
+        }
+        public void formateoNumeros(string numeros_sinformatear)
+        {
+
+
+            var milista = new List<float>();
+           var numeros_formateados = numeros_sinformatear.Split(',').ToList();
+
+            foreach (var numero in numeros_formateados)
+
+            {
+                try
+                {
+                    var num = Convertir_Cultura(numero);
+                   
+                    milista.Add(num);
+                }
+                catch (FormatException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    break;
+
+                }
+              
+
+
+            }
+            _numbers = milista;
+
+
+        }
+
+        private static float Convertir_Cultura(string numero)
+        {
+            var num = 0f;
+            var style = NumberStyles.Float;
+            var culture = CultureInfo.CreateSpecificCulture("en-EN");
+            float.TryParse(numero, style, culture, out num);
+            return num;
+        }
+
+        public void AddFun()
+        {
+            var result = 0F;
+
+            foreach (var number in _numbers)
+            {
+                result += number;
+            }
+            Console.WriteLine(result);
+        }
+        public void DivFun()
+        {
+            if (_numbers.Count == 2 )
+            {
+                if (_numbers.Last() == 0l)
+                {
+                    Console.WriteLine("No se puede dividir entre cero");
+                }
+                var result = _numbers.First()/_numbers.Last();
+
+                Console.WriteLine(result);
+            }
+         
+            else
+            {
+                Console.WriteLine("Por favor, introduzca SOLO dos números");
+            }
+        }
+       
+        
+        
+        public  bool ContieneLetras(String str)
+        {
+            return Regex.IsMatch(str, @"^[a-zA-Z]+$");
+        }
+        public bool ValidateAll(object[] numbers)
+        {
+          var  result = true;
+
+            foreach (var number in numbers)
+            {
+                if (ContieneLetras(number.ToString()))
+                {
+                    result = false;
+                } 
+            }
+            return result;
+            
+        }
+
+    }
+}
--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -14,117 +14,57 @@ namespace Prueba
     {
         static void Main(string[] args)
         {
+            Calc mi_calc = new Calc();
             Console.WriteLine("Introduzca opción: ");
             var opcion = Console.ReadLine();
-            while (opcion.ToUpper()!="EXIT")
-            { 
+            while (opcion.ToUpper() != "EXIT")
+            {
 
                 if (opcion.ToUpper() == "SUM")
                 {
                     Console.WriteLine("Introduzca argumentos para la suma separados por coma y pulse intro ");
                     var numeros = Console.ReadLine();
-                    if (!ContieneLetras(numeros))
+
+
+
+                    if (!mi_calc.ContieneLetras(numeros))
                     {
-                        var milista = formateoNumeros(numeros);
-                        if (milista.Any())
-                            Suma(milista);
+                        mi_calc.formateoNumeros(numeros);
+                        if (mi_calc.numbers.Any())
+
+                            mi_calc.AddFun();
+
                     }
                     else
                     {
-                        Console.WriteLine("Error formatting arguments. La cadena de entrada no tiene el formato correcto");
+                        Console.WriteLine(
+                            "Error formatting arguments. La cadena de entrada no tiene el formato correcto");
                     }
-                   
+
                 }
-               else if (opcion.ToUpper() == "DIV")
+                else if (opcion.ToUpper() == "DIV")
                 {
                     Console.WriteLine("Introduzca argumentos para la división separados por coma y pulse intro ");
                     var numeros = Console.ReadLine();
-                    if (ContieneLetras(numeros))
+                    if (!mi_calc.ContieneLetras(numeros))
                     {
-                        var milista = formateoNumeros(numeros);
-                        if(milista.Any())
-                        Division(milista);
+                        mi_calc.formateoNumeros(numeros);
+                        if (mi_calc.numbers.Any())
+                            mi_calc.DivFun();
                     }
                     else
                     {
-                        Console.WriteLine("Error formatting arguments. La cadena de entrada no tiene el formato correcto");
+
+
                     }
-                  
                 }
                 else
                 {
                     Console.WriteLine("Opción " + opcion + " No valida");
                 }
                 Console.WriteLine("Introduzca opción: ");
-                 opcion = Console.ReadLine();
+                opcion = Console.ReadLine();
             }
-         
-        }
-        private static bool ContieneLetras(String str)
-        {
-            return Regex.IsMatch(str, @"^[a-zA-Z]+$");
-        }
-        static List<float> formateoNumeros(string numeros_sinformatear)
-        {
-           
-            var resultado= new List<float>();
-         
-            var numeros_formateados = numeros_sinformatear.Split(',').ToList();
-           
-            foreach (var numero in numeros_formateados)
-
-            {
-                try
-                {
-                  var num=  Convertir_Cultura(numero);
-                    resultado.Add(num);
-                }
-                catch (FormatException ex)
-                {
-                    
-                    Console.WriteLine(ex.Message);
-                    break;
-
-                }
-               
-             
-            }
-          
-
-            return resultado;
-        }
-
-        private static float Convertir_Cultura(string numero)
-        {
-            var num = 0f;
-            var style = NumberStyles.Float;
-            var culture = CultureInfo.CreateSpecificCulture("en-EN");
-            float.TryParse(numero, style, culture, out num);
-            return num;
-        }
-
-        static void Suma(List<float> numeros_decimales)
-        {
-            var sum = 0f;
-            foreach (var numerosDecimale in numeros_decimales)
-            {
-                sum += numerosDecimale;
-            }
-            Console.WriteLine("La suma de los operandos es " + sum);
-        }
-        static void Division(List<float> numeros_decimales)
-        {
-            if (numeros_decimales.Count > 2)
-            {
-                Console.WriteLine("Por favor, introduzca SOLO dos números");
-            }
-            else
-            {
-                var resultado = numeros_decimales[0]/numeros_decimales[1];
-                Console.WriteLine("La division de los operandos es " + resultado);
-
-            }
-
 
         }
     }
